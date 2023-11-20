c2bd1c9356884b66d7e97c873de59a55cf189462
maria_clara <maria.siqueira70@aluno.ce.gov.br>
2023-11-20 14:45:04 -0300

    v1
    conexao

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>acessar sua conta </h1>
+    <form action="cadastro.php">
+        <p>
+            <label>Email</label>
+            <input type=email name="email">
+        </p>
+        <p>
+            <label>Senha</label>
+            <input type=senha name="senha">
+        </p>
+        <p>
+            <button>Enviar</button>
+
+</form>
+</body>
+</html>
